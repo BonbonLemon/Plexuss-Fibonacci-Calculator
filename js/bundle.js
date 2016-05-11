fe6cf04d84e9d20857a2dd3facd68323f033938b
Stephen Chen <chen.stephen.r@gmail.com>
2016-05-11 12:30:57 -0700

    prints fibonacci sequence

--- js/bundle.js
+++ js/bundle.js
@@ -52,6 +52,8 @@
 	
 	    if (isNaN(inputLength)) {
 	      $('#flash-error').text('Input is not a number')
+	    } else if (inputLength <= 0) {
+	      $('#flash-error').text('Input must be greater than 0')
 	    } else {
 	      $('#flash-error').text('')
 	
@@ -61,7 +63,20 @@
 	});
 	
 	var calculateFibs = function (length) {
-	  console.log(length);
+	  var fibs;
+	
+	  if (length === 1) {
+	    fibs = [0];
+	  } else {
+	    fibs = [0, 1];
+	    for (var i = 2; i < length; i++) {
+	      // debugger;
+	      var nextFib = fibs[i - 2] + fibs[i - 1];
+	      fibs.push(nextFib)
+	    }
+	  }
+	
+	  $('#fib-list').text(fibs.join(", "));
 	};
 
 
--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
 
     if (isNaN(inputLength)) {
       $('#flash-error').text('Input is not a number')
+    } else if (inputLength <= 0) {
+      $('#flash-error').text('Input must be greater than 0')
     } else {
       $('#flash-error').text('')
 
@@ -15,5 +17,18 @@ $(document).ready(function () {
 });
 
 var calculateFibs = function (length) {
-  console.log(length);
+  var fibs;
+
+  if (length === 1) {
+    fibs = [0];
+  } else {
+    fibs = [0, 1];
+    for (var i = 2; i < length; i++) {
+      // debugger;
+      var nextFib = fibs[i - 2] + fibs[i - 1];
+      fibs.push(nextFib)
+    }
+  }
+
+  $('#fib-list').text(fibs.join(", "));
 };
